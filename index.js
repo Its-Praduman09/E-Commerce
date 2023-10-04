9fa69fde56699a4e3a32bfa9b074faecaf09b62b
Its-Praduman09 <78318138+Its-Praduman09@users.noreply.github.com>
2023-10-04 19:47:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function Run()
+{
+    setTimeout(play,3000);
+    function play()
+    {
+    document.getElementById('logo').style.display='block';
+    document.getElementById('firstlogo').style.display='none';
+    }
+}
